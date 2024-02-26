802e5d9ecab9b82d0cce7368dfbfb59a1fbf41dd
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-26 16:01:36 +0600

    coupon and discount step done

--- js/onClickProducts.js
+++ js/onClickProducts.js
@@ -1,14 +1,42 @@
 // click events
 
-var total=0;
-function onClickProducts(productId){
-    const getValueFromProduct=getValue(productId);
-  
-     total=total+getValueFromProduct;
-   
-    setValue('totalPrice',total);
-    // discount price set
-    const afterDiscountPrice=discountPrice(total,20);
-    setValue('FinalTotal',afterDiscountPrice);
-   
+var total = 0;
+function onClickProducts(productId) {
+    const getValueFromProduct = getValue(productId);
+
+    total = total + getValueFromProduct;
+
+    setValue('totalPrice', total);
+
+    setValue('FinalTotal', total);
+
+
+    return total;
 }
+
+// apply button click
+function applyBones(inputId) {
+    const textValue = document.getElementById(inputId).value;
+
+    // discount price set
+    const beforeDiscountTotal = document.getElementById('totalPrice').innerText;
+    const afterDiscountPrice = discountPrice(beforeDiscountTotal, 20);
+    setValue('FinalTotal', afterDiscountPrice);
+
+    if (textValue === 'sale200') {
+
+        setValue('FinalTotal', afterDiscountPrice);
+        // console.log('success', textValue)
+
+    }
+    else {
+
+        setValue('totalPrice', total);
+    }
+
+
+}
+
+
+
+
